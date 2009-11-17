ffe5d57a824f3daa0cbcec7d7541a86217d2f29c
krishna.pothula <krishna.pothula@f0b6d886-d066-11de-9e88-ff79fe1e0c1e>
2009-11-17 12:01:46 +0000

    Commiting a workable code. Needs lot of refactoring and cleanup

--- trunk/src/in/kp/doclet/ClassBeanCreator.java
+++ trunk/src/in/kp/doclet/ClassBeanCreator.java
@@ -1,6 +1,5 @@
 package in.kp.doclet;
 
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +29,7 @@ public class ClassBeanCreator {
 		List<String> methodsList = loadMethodsList(methods);
 		for (int i = 0; i < methods.length; i++) {
 			MethodDoc method = methods[i];
-			if (method.isStatic() || method.isPrivate() || method.isProtected()) {
+			if (method.isStatic() || method.isPrivate() || method.isProtected() || method.parameters().length > 1) {
 				System.out.println(method.name() + " skipped");
 			} else if (method.name().startsWith("set")) {
 				String setterName = method.name().substring(3);
@@ -47,20 +46,24 @@ public class ClassBeanCreator {
 
 	private void createTestForSetter(MethodDoc method, String pMethodName) {
 		MethodBean methodBean = new MethodBean("setAndGet" + pMethodName);
-		classBean.addImport("org.junit.Test");
 		methodBean.setAnnotation(CodeGenConstants.ANN_TEST);
 		methodBean.setModifiers("public final void ");
 		Parameter[] params = method.parameters();
 		if (params.length == 1) {
 			Parameter param = params[0];
 			Type paramType = param.type();
-			String paramTypeClass = paramType.qualifiedTypeName();
-			if (isImport(paramTypeClass)) {
-				methodBean.addMethodImport(paramTypeClass);
+			if (isImport(paramType)) {
+				addImport(methodBean,paramType.qualifiedTypeName());
 			}
-			methodBean.addToBody(DataTypeTestValues.generateCode(paramTypeClass));
+			methodBean.addToBody(CodeGenConstants.DATA_COMMENT);
+			String testValues = DataTypeTestValues.generateCode(paramType);
+			if(testValues == null || testValues.equals("")){
+				return;
+			}
+			methodBean.addToBody(testValues);
 			String className = classDoc.name();
 			String classVarName = "m" + className;
+			methodBean.addToBody("//creating instance for the class to be tested");
 			methodBean.addToBody(className +" "+ classVarName + " = new " + className + "();");
 			methodBean.addToBody("for (int i = 0; i < tests.length; i++) {");
 			methodBean.addToBody(classVarName + ".set" + pMethodName + "(tests[i]);");
@@ -69,13 +72,29 @@ public class ClassBeanCreator {
 		}
 		classBean.addMethod(methodBean);
 	}
+	
+	private void addImport(MethodBean methodBean, String className) {
+		methodBean.addMethodImport(className);
+		if(className.equals("java.util.List"))
+			methodBean.addMethodImport("java.util.ArrayList");
+		else if(className.equals("java.util.Map"))
+			methodBean.addMethodImport("java.util.HashMap");
+	}
 
-	private boolean isImport(String className) {
-		if (className == null || className.startsWith("java.lang")) {
+	private boolean isImport(Type paramType) {
+		String className = paramType.qualifiedTypeName();
+		if (paramType.isPrimitive() || className.startsWith("java.lang") || className.equals("java.sql.Date")) {
 			return false;
 		} else {
 			return true;
 		}
+	}
+	
+	private boolean isPrimitive(String dataType) {
+		if("long".equals(dataType) || "int".equals(dataType) || "double".equals(dataType) || "char".equals(dataType) || "float".equals(dataType))
+			return true;
+		else
+			return false;
 	}
 
 	private List<String> loadMethodsList(MethodDoc[] methods) {
@@ -88,6 +107,7 @@ public class ClassBeanCreator {
 
 	private void addDefaultImports() {
 		classBean.addImport("org.junit.Assert.*");
+		classBean.addImport("org.junit.Test");
 		classBean.addImport("static org.junit.Assert.assertEquals");
 		classBean.addImport("static org.junit.Assert.assertTrue");
 	}
--- trunk/src/in/kp/doclet/JUnitCodeGen.java
+++ trunk/src/in/kp/doclet/JUnitCodeGen.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Properties;
 
 import com.sun.javadoc.*;
@@ -13,99 +15,126 @@ import com.sun.javadoc.*;
  * 
  */
 public class JUnitCodeGen {
-    private static String outDir = null;
+	private static String outDir = null;
+	private static boolean overwriteDestDir = false;
 
-    private static void init(RootDoc root) {
-        outDir = readOptions(root.options());
-        
-    }
+	private static void init(RootDoc root) {
+		outDir = readOptions(root.options());
 
-    public static boolean start(RootDoc root) {
-        try {
-            init(root);
-            ClassDoc[] classes = root.classes();
-            for (int i = 0; i < classes.length; i++) {
-                ClassDoc classDoc = classes[i];
-                if (classDoc.isAbstract() || classDoc.isInterface() || classDoc.isEnum() || classDoc.isPrivate()
-                        || classDoc.isAnnotationType()) {
-                } else if (!hasDefaultConstructor(classDoc)) {
-                    System.out.println(classDoc.qualifiedTypeName()+" does not have default Constructor.");
-                } else {
-                    ClassBean testClassBean = ClassBeanCreator.create(classDoc);
-                    JavaCodeWriter writer = getWriter(testClassBean);
-                    JavaCodeGenerator.generate(testClassBean, writer);
-                    writer.close();
-                }
-            }
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	}
 
-    private static boolean hasDefaultConstructor(ClassDoc classDoc) {
-        ConstructorDoc[] cdocs = classDoc.constructors();
-        for (int i = 0; i < cdocs.length; i++) {
-            ConstructorDoc cdoc = cdocs[i];
-            if (cdoc.parameters().length == 0) {
-                if (!cdoc.isPrivate()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	public static boolean start(RootDoc root) {
+		try {
+			init(root);
+			ClassDoc[] classes = root.classes();
+			ArrayList<String> generatesTestClasses = new ArrayList<String>();
+			for (int i = 0; i < classes.length; i++) {
+				ClassDoc classDoc = classes[i];
+				if (classDoc.isAbstract() || classDoc.isInterface() || classDoc.isEnum() || classDoc.isPrivate() || classDoc.isAnnotationType()) {
+				} else if (!hasDefaultConstructor(classDoc)) {
+					System.out.println(classDoc.qualifiedTypeName() + " does not have default Constructor.");
+				} else {
+					ClassBean testClassBean = ClassBeanCreator.create(classDoc);
+					if (testClassBean.hasMethods()) {
+						JavaCodeWriter writer = getWriter(testClassBean);
+						if(writer != null) {
+							JavaCodeGenerator.generate(testClassBean, writer);
+							generatesTestClasses.add(classDoc.qualifiedTypeName());
+							writer.close();
+						}
+					} else {
+						System.out.println(classDoc.qualifiedTypeName() + " does not have any methods to cover.");
+					}
+				}
+			}
+			System.out.println("---------REPORT-----------------------------------------------");
+			for (Iterator iterator = generatesTestClasses.iterator(); iterator.hasNext();) {
+				String clsName = (String) iterator.next();
+				System.out.println("\t"+clsName);
+			}
+			System.out.println("-------------------------------------------------------------");
+			
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-    private static JavaCodeWriter getWriter(ClassBean classBean) throws IOException {
-        String packagePath = classBean.getPackageName();
-        packagePath = packagePath.replace('.', '/');
-        File packageDirs = new File(outDir, packagePath);
-        packageDirs.mkdirs();
-        File testFile = new File(packageDirs.getAbsoluteFile(), classBean.getName()+".java");
-        BufferedWriter out = new BufferedWriter(new FileWriter(testFile, false));
-        return new JavaCodeWriter(out);
-    }
+	private static boolean hasDefaultConstructor(ClassDoc classDoc) {
+		ConstructorDoc[] cdocs = classDoc.constructors();
+		for (int i = 0; i < cdocs.length; i++) {
+			ConstructorDoc cdoc = cdocs[i];
+			if (cdoc.parameters().length == 0) {
+				if (!cdoc.isPrivate()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    private static String readOptions(final String[][] options) {
-        Properties p = new Properties(System.getProperties());
-        String outputDir = null;
-        for (int i = 0; i < options.length; i++) {
-            String[] opt = options[i];
-            if (opt[0].equals("-d")) {
-                outputDir = opt[1];
-            }
-        }
-        if (outputDir == null) {
-            outputDir = p.getProperty("user.dir");
-        }
-        return outputDir;
-    }
+	private static JavaCodeWriter getWriter(ClassBean classBean) throws IOException {
+		String packagePath = classBean.getPackageName();
+		packagePath = packagePath.replace('.', '/');
+		File packageDirs = new File(outDir, packagePath);
+		packageDirs.mkdirs();
+		File testFile = new File(packageDirs.getAbsoluteFile(), classBean.getName() + ".java");
+		if(testFile.exists()) {
+			System.out.println("File already exists");
+			return null;
+		}
+		BufferedWriter out = new BufferedWriter(new FileWriter(testFile, false));
+		return new JavaCodeWriter(out);
+	}
 
-    public static int optionLength(String option) {
-        if (option.equals("-d")) {
-            return 2;
-        }
-        return 0;
-    }
+	private static String readOptions(final String[][] options) {
+		Properties p = new Properties(System.getProperties());
+		String outputDir = null;
+		for (int i = 0; i < options.length; i++) {
+			String[] opt = options[i];
+			if (opt[0].equals("-d")) {
+				outputDir = opt[1];
+			} else if (opt[0].equals("-overwrite")) {
+				overwriteDestDir = Boolean.valueOf(opt[1]);
+			}
+		}
+		if (outputDir == null) {
+			outputDir = p.getProperty("user.dir");
+		}
+		return outputDir;
+	}
 
-    public static boolean validOptions(String options[][], DocErrorReporter reporter) {
-        boolean foundDirOption = false;
-        for (int i = 0; i < options.length; i++) {
-            String[] opt = options[i];
-            if (opt[0].equals("-d")) {
-                if (foundDirOption) {
-                    reporter.printError("Only one -d option allowed.");
-                    return false;
-                } else {
-                    foundDirOption = true;
-                }
-            }
-        }
-        if (!foundDirOption) {
-            reporter.printError("Usage: javadoc -d outputDir " + "-doclet in.kp.doclet.sample.ListClass " + "... ");
-        }
-        return foundDirOption;
+	public static int optionLength(String option) {
+		if (option.equals("-d")) {
+			return 2;
+		}
+		if (option.equals("-overwrite")) {
+			return 2;
+		}
+		return 0;
+	}
 
-    }
+	public static boolean validOptions(String options[][], DocErrorReporter reporter) {
+		boolean foundDirOption = false;
+		for (int i = 0; i < options.length; i++) {
+			String[] opt = options[i];
+			if (opt[0].equals("-d")) {
+				if (foundDirOption) {
+					reporter.printError("Only one -d option allowed.");
+					return false;
+				} else {
+					foundDirOption = true;
+				}
+			} else if (opt[0].equals("-overwrite")) {
+				
+			}
+
+		}
+		if (!foundDirOption) {
+			reporter.printError("Usage: javadoc -d outputDir " + "-doclet in.kp.doclet.sample.ListClass " + "... ");
+		}
+		return foundDirOption;
+
+	}
 }
